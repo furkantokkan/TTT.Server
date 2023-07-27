37cef560354fad2aef20f98bdc7adda6da84955b
Furkan Tokkan <efurk64@gmail.com>
2023-07-27 04:37:59 +0300

    Find and cancel Opponent request

---
+++ ServerPacketHandlers/CancelFindOpponentRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using NetworkShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTT.Server.NetworkShared;
+using TTT.Server.NetworkShared.Attributes;
+using TTT.Server.PacketHandlers;
+
+namespace TTT.Server.ServerPacketHandlers
+{
+    [HandlerRegister(PacketType.CancelFindOpponentRequest)]
+    public class CancelFindOpponentRequestHandler : IPacketHandler
+    {
+        private readonly ILogger<CancelFindOpponentRequestHandler> logger;
+        public CancelFindOpponentRequestHandler(ILogger<CancelFindOpponentRequestHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Handle(INetPacket packet, int connectionID)
+        {
+            logger.LogInformation("Recived cancel find opponent request!");
+        }
+    }
+}
---
+++ ServerPacketHandlers/FindOpponentRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using NetworkShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTT.Server.NetworkShared;
+using TTT.Server.NetworkShared.Attributes;
+
+namespace TTT.Server.PacketHandlers
+{
+    [HandlerRegister(PacketType.FindOpponentRequest)]
+    public class FindOpponentRequestHandler : IPacketHandler
+    {
+        private readonly ILogger<FindOpponentRequestHandler> logger;
+        public FindOpponentRequestHandler(ILogger<FindOpponentRequestHandler> logger)
+        {
+            this.logger = logger;
+        }
+        public void Handle(INetPacket packet, int connectionID)
+        {
+            logger.LogInformation("Recived Find Opponent Request");
+        }
+    }
+}
